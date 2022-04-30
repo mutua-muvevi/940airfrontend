0ff86c806b5e4d6ca6a670a1a7e9689a194c01bf
SamsAgency <samstore637@gmail.com>
2022-04-30 15:32:36 +0300

    added navbar

--- src/components/landing/navbar/navbarcontent.js
+++ src/components/landing/navbar/navbarcontent.js
@@ -2,6 +2,10 @@ import HomeIcon from '@mui/icons-material/Home';
 import InfoIcon from '@mui/icons-material/Info';
 import ContactSupportIcon from '@mui/icons-material/ContactSupport';
 
+
+import LockIcon from '@mui/icons-material/Lock';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+
 export const navbarContent = {
 	logo: {
 
@@ -10,17 +14,34 @@ export const navbarContent = {
 		{
 			text: "home",
 			path: "/landing/home",
-			icon: <HomeIcon/>
+			icon: <HomeIcon color="primary"/>,
+			index: 0
 		},
 		{
 			text: "about",
 			path: "/landing/about",
-			icon: <InfoIcon/>
+			icon: <InfoIcon color="primary"/>,
+			index: 1
 		},
 		{
 			text: "contact",
 			path: "/landing/contact",
-			icon: <ContactSupportIcon/>
+			icon: <ContactSupportIcon color="primary"/>,
+			index: 2
+		},
+	],
+	navActionItems : [
+		{
+			text: "login",
+			path: "/landing/home",
+			icon: <LockIcon color="primary"/>,
+			index: 3
+		},
+		{
+			text: "signup",
+			path: "/landing/home",
+			icon: <HowToRegIcon color="primary"/>,
+			index: 4
 		},
 	]
 }
