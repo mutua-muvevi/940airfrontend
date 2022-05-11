cf14c68a6e694a2242f7be66f3c99bf422f4a27c
SamsAgency <samstore637@gmail.com>
2022-05-11 17:34:47 +0300

    started working on profile

---
+++ src/pages/dashboard/sender/profile/profileeditform.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+import { Box, Button, ButtonGroup, Grid } from "@mui/material";
+import { styled } from "@mui/system";
+import SendIcon from '@mui/icons-material/Send';
+
+import { profileEditFormContent } from "./profileeditform"
+import AlertModal from "../../../../layout/modal/alertmodal/alertmodal";
+import TextFieldWrapper from "../../../../components/formui/textfield";
+
+const ProfileEditForm = () => {
+	return (
+		<Box>
+
+		</Box>
+	)
+}
+
+export default ProfileEditForm
